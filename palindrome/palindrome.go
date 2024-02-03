d1e3252dd3abf13a918e812d37360e7d8c19f680
Siravitt <siravitt.tan@gmail.com>
2024-02-03 22:08:06 +0700

    case: input 9999 return true

--- palindrome/palindrome.go
+++ palindrome/palindrome.go
@@ -1,9 +1,26 @@
 package palindrome
 
+import (
+	"strconv"
+)
+
 // Input: x = 121
 // Output: true
 // Explanation: 121 reads as 121 from left to right and from right to left.
 
 func Palindrome(n int) bool {
+	s := strconv.Itoa(n)
+	if len(s) == 1 {
+		return true
+	}
+
+	for i := 0; i < len(s); i++ {
+		if string(s[i]) != "" && string(s[len(s)-1-i]) != "" {
+			if s[i] != s[len(s)-i-1] {
+				return false
+			}
+		}
+	}
+
 	return true
 }
--- palindrome/palindrome_test.go
+++ palindrome/palindrome_test.go
@@ -11,3 +11,23 @@ func TestOneChar(t *testing.T) {
 		t.Fatalf("input: %v want %v but got %v", input, want, res)
 	}
 }
+
+func TestRepeatChar(t *testing.T) {
+	input := 11
+	want := true
+
+	res := Palindrome(input)
+	if res != want {
+		t.Fatalf("input: %v want %v but got %v", input, want, res)
+	}
+}
+
+func TestNonRepeatChar(t *testing.T) {
+	input := 9999
+	want := true
+
+	res := Palindrome(input)
+	if res != want {
+		t.Fatalf("input: %v want %v but got %v", input, want, res)
+	}
+}
