22248f52a82d5a4023ebf88f580eea30fb3ed8f7
Siravitt <siravitt.tan@gmail.com>
2024-02-05 17:41:45 +0700

    add: len / 2

--- palindrome/palindrome.go
+++ palindrome/palindrome.go
@@ -12,7 +12,7 @@ import (
 func Palindrome(n int) bool {
 	s := strconv.Itoa(n)
 
-	for i := 0; i < int(math.Floor(float64(len(s)))); i++ {
+	for i := 0; i < int(math.Floor(float64(len(s)/2))); i++ {
 		if s[i] != s[len(s)-i-1] {
 			return false
 		}
